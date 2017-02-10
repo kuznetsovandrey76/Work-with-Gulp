b0b5658b703588b52e8dde10053a4acaa467fdbe
kuznetsovandrey76 <and.rey.q@yandex.ru>
2017-02-10 10:01:30 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var gulp = require('gulp'),
+	cleanCSS = require('gulp-clean-css'),
+	livereload = require('gulp-livereload');
+
+
+gulp.task('html', function() {
+	return gulp.src('app/index.html')  // какой файл менять
+    .pipe(gulp.dest('dist')) // куда сохранить (ex: ./ - корень)
+    .pipe(livereload());
+});
+
+gulp.task('css', function() {
+	return gulp.src('app/css/*.css')  
+	.pipe(cleanCSS())  
+    .pipe(gulp.dest('dist'))
+    .pipe(livereload());
+});
+
+gulp.task('js', function() {
+	return gulp.src('app/js/*.js')  
+    .pipe(gulp.dest('dist'))
+    .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+	livereload.listen();
+	gulp.watch('app/index.html', ['html']);
+	gulp.watch('app/css/*.css', ['css']);
+	gulp.watch('app/js/*.js', ['js']);
+});
+
+gulp.task('default', ['html', 'css', 'js', 'watch']);
